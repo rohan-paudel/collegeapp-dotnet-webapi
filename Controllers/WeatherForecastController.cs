62a78f42dd38d938c94d6f7041fec33a4a5ab706
rohan-paudel <developer.rpaudel@gmail.com>
2024-02-03 13:33:30 +0545

    added to add roles.

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -1,32 +1,67 @@
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.JSInterop.Infrastructure;
 
 namespace CollegeAppDotnetWebApi.Controllers;
 
 [ApiController]
-[Route("[controller]")]
+[Route("[controller]/[Action]")]
 public class WeatherForecastController : ControllerBase
 {
     private static readonly string[] Summaries = new[]
     {
-        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        "Freezing",
+        "Bracing",
+        "Chilly",
+        "Cool",
+        "Mild",
+        "Warm",
+        "Balmy",
+        "Hot",
+        "Sweltering",
+        "Scorching"
     };
 
     private readonly ILogger<WeatherForecastController> _logger;
+    private readonly UserManager<TejiloUser> _userManager;
+    private readonly RoleManager<IdentityRole> _roleManager;
 
-    public WeatherForecastController(ILogger<WeatherForecastController> logger)
+    public WeatherForecastController(
+        ILogger<WeatherForecastController> logger,
+        UserManager<TejiloUser> userManager,
+        RoleManager<IdentityRole> roleManager
+    )
     {
         _logger = logger;
+        _userManager = userManager;
+        _roleManager = roleManager;
     }
 
     [HttpGet(Name = "GetWeatherForecast")]
     public IEnumerable<WeatherForecast> Get()
     {
-        return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+        return Enumerable
+            .Range(1, 5)
+            .Select(
+                index =>
+                    new WeatherForecast
+                    {
+                        Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                        TemperatureC = Random.Shared.Next(-20, 55),
+                        Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+                    }
+            )
+            .ToArray();
+    }
+
+    [HttpGet]
+    public async Task<string> SetRole()
+    {
+        foreach (var role in Roles.identityRoles)
         {
-            Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            TemperatureC = Random.Shared.Next(-20, 55),
-            Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-        })
-        .ToArray();
+            await _roleManager.CreateAsync(role).ConfigureAwait(false);
+        }
+
+        return "done";
     }
 }
--- DatabaseModels/RoleModel/Roles.cs
+++ DatabaseModels/RoleModel/Roles.cs
@@ -1,4 +1,6 @@
-﻿namespace CollegeAppDotnetWebApi;
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace CollegeAppDotnetWebApi;
 
 public static class Roles
 {
@@ -9,4 +11,15 @@ public static class Roles
     public const string TejiloSubAdmin = "TejiloSubAdmin";
     public const string TejiloCustomerCare = "TejiloCustomerCare";
     public const string TejiloMarketing = "TejiloMarketing";
+
+    public static List<IdentityRole> identityRoles =
+    [
+        new IdentityRole(User),
+        new IdentityRole(CollegeAdmin),
+        new IdentityRole(TejiloSuperAdmin),
+        new IdentityRole(TejiloAdmin),
+        new IdentityRole(TejiloSubAdmin),
+        new IdentityRole(TejiloCustomerCare),
+        new IdentityRole(TejiloMarketing)
+    ];
 }
