7f9ceba3fba66b71999571a5cc26c91e34f0f8b3
rohan-paudel <developer.rpaudel@gmail.com>
2024-02-03 12:40:32 +0545

    package is added and added connection string

---
+++ SetupConstants/SqlSetupConstants.cs
@@ -0,0 +1,7 @@
+﻿namespace CollegeAppDotnetWebApi;
+
+public static class SqlSetupConstants
+{
+    public static string ConnectionString =
+        "server=152.67.165.226; user=rohan; password=Jeevan72779673ok@#; database=crudoperation; port=3306; SslMode=Required; Allow User Variables=true";
+}
