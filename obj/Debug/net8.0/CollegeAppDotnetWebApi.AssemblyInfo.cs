17d251861b2cf9a97e1a5ce76c7ba859c336181e
rohan-paudel <developer.rpaudel@gmail.com>
2024-02-03 12:41:49 +0545

    builded

--- obj/Debug/net8.0/CollegeAppDotnetWebApi.AssemblyInfo.cs
+++ obj/Debug/net8.0/CollegeAppDotnetWebApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CollegeAppDotnetWebApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7f9ceba3fba66b71999571a5cc26c91e34f0f8b3")]
 [assembly: System.Reflection.AssemblyProductAttribute("CollegeAppDotnetWebApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CollegeAppDotnetWebApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
