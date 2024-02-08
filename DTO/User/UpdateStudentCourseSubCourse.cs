55abdf854279ab5ae9059943330fc14df56cbb2a
rohan-paudel <developer.rpaudel@gmail.com>
2024-02-08 20:36:52 +0545

    added Update student course and sub course

---
+++ DTO/User/UpdateStudentCourseSubCourse.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CollegeAppDotnetWebApi;
+
+public class UpdateStudentCourseSubCourse
+{
+    [Required]
+    public string StudentId { get; set; } = "";
+
+    [Required]
+    public string CourseId { get; set; } = "";
+
+    [Required]
+    public string SubCourseId { get; set; } = "";
+}
