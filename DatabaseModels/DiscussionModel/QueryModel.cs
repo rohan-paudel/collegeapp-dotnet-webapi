089a9315095a7da96878ccb2054836fc33c20c51
rohan-paudel <developer.rpaudel@gmail.com>
2024-02-12 16:05:58 +0545

    added new migration

--- DatabaseModels/DiscussionModel/QueryModel.cs
+++ DatabaseModels/DiscussionModel/QueryModel.cs
@@ -1,8 +1,10 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
 
 namespace CollegeAppDotnetWebApi;
 
+[Index(nameof(DiscussionId))]
 public class QueryModel
 {
     [Key]
