6bd444f43800bbee48d4025fd769418577f3b3c3
rohan-paudel <developer.rpaudel@gmail.com>
2024-02-14 19:19:34 +0545

    dotnet added

--- obj/Debug/net8.0/CollegeAppDotnetWebApi.AssemblyInfo.cs
+++ obj/Debug/net8.0/CollegeAppDotnetWebApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CollegeAppDotnetWebApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8e214b67c49c2981481b4d92a6abbf9bd59d0e26")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e33c1a2bec572f2a7c8fcaa5258e452ad898b48d")]
 [assembly: System.Reflection.AssemblyProductAttribute("CollegeAppDotnetWebApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CollegeAppDotnetWebApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
