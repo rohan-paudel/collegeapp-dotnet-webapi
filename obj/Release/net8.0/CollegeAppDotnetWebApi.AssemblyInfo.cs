6e4bf15a8d853b51a0f467595aac5405d51bf76b
rohan-paudel <developer.rpaudel@gmail.com>
2024-02-26 18:07:34 +0545

    updated

--- obj/Release/net8.0/CollegeAppDotnetWebApi.AssemblyInfo.cs
+++ obj/Release/net8.0/CollegeAppDotnetWebApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CollegeAppDotnetWebApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+871d2828d84bdf87cc9b62d39c4cdb5b6461ed8e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f2f5940ab13f40dacd73bb457ed941432ff1fe6e")]
 [assembly: System.Reflection.AssemblyProductAttribute("CollegeAppDotnetWebApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CollegeAppDotnetWebApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
