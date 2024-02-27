21238efda596c8bc7fe936c09bc66b032780b2a3
rohan-paudel <developer.rpaudel@gmail.com>
2024-02-27 11:07:55 +0545

    dotnet completed

--- Controllers/UserControllers/UserNoteManagementController.cs
+++ Controllers/UserControllers/UserNoteManagementController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Http.HttpResults;
+﻿using System.Security.Claims;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CollegeAppDotnetWebApi;
@@ -15,28 +17,37 @@ public class UserNoteManagementController : ControllerBase
     }
 
     [HttpPost]
+    [Authorize(Policy = "PolicyForMobileDevice")]
     public async Task<
         Results<Ok<ResponseDTO<string>>, BadRequest<ResponseDTO<string>>>
     > SetUserNote(UserNoteRequestDTO userNoteRequestDTO)
     {
+        userNoteRequestDTO.StudentId = User.FindFirstValue(ClaimTypes.NameIdentifier)!;
         return await _userNoteManagementDL.SetUserNote(userNoteRequestDTO).ConfigureAwait(false);
     }
 
     [HttpGet]
+    [Authorize(Policy = "PolicyForMobileDevice")]
     public async Task<
         Results<Ok<ResponseDTO<IEnumerable<UserNoteResponseDTO>>>, BadRequest<ResponseDTO<string>>>
-    > GetUserNotes(int topicId, string studentId, bool? noteStatus)
+    > GetUserNotes(int topicId)
     {
         return await (
-            _userNoteManagementDL.GetUserNotes(topicId, studentId, noteStatus)
+            _userNoteManagementDL.GetUserNotes(
+                topicId,
+                User.FindFirstValue(ClaimTypes.NameIdentifier)!,
+                true
+            )
         ).ConfigureAwait(false);
     }
 
     [HttpPost]
+    [Authorize(Policy = "PolicyForMobileDevice")]
     public async Task<
         Results<Ok<ResponseDTO<string>>, BadRequest<ResponseDTO<string>>>
     > EditUserNote(UserNoteUpdateDTO userNoteUpdateDTO)
     {
+        userNoteUpdateDTO.StudentId = User.FindFirstValue(ClaimTypes.NameIdentifier)!;
         return await _userNoteManagementDL.EditUserNote(userNoteUpdateDTO).ConfigureAwait(false);
     }
 }
--- obj/Release/net8.0/CollegeAppDotnetWebApi.AssemblyInfo.cs
+++ obj/Release/net8.0/CollegeAppDotnetWebApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CollegeAppDotnetWebApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+21f4ef4b99848b0913961c11d1bdfa9a54866dc3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4938e00c276e0ad5d2e8fc2231f740ddf315ae72")]
 [assembly: System.Reflection.AssemblyProductAttribute("CollegeAppDotnetWebApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CollegeAppDotnetWebApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
